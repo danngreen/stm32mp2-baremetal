05fe5855b87a18946e375805e8b832405c22f62e
Dan Green <danngreen1@gmail.com>
2026-07-28 22:30:47 -0700

    m33 interrupt handlers in shared/

---
+++ shared/interrupt_m33/interrupt_control.hh
@@ -0,0 +1,34 @@
+#pragma once
+#include "stm32mp2xx.h"
+
+struct InterruptControl {
+	InterruptControl() = delete;
+
+	static void SetVectorTable(uint32_t reset_address)
+	{
+		SCB->VTOR = reset_address & (uint32_t)0x1FFFFF80;
+	}
+
+	static void set_irq_priority(IRQn_Type irqn, uint32_t pri1, uint32_t pri2)
+	{
+		auto pri = NVIC_EncodePriority(NVIC_GetPriorityGrouping(), pri1, pri2);
+		NVIC_SetPriority(irqn, pri);
+	}
+
+	static void disable_irq(IRQn_Type irqn)
+	{
+		NVIC_DisableIRQ(irqn);
+	}
+
+	// TriggerType is here to enable CA7<=>CM4 compatibility
+	enum TriggerType { LevelTriggered = 0b01, EdgeTriggered = 0b10 };
+	static void enable_irq(IRQn_Type irqn, TriggerType _ignored = EdgeTriggered)
+	{
+		NVIC_EnableIRQ(irqn);
+	}
+
+	static void reenable_irq(IRQn_Type irqn)
+	{
+		NVIC_EnableIRQ(irqn);
+	}
+};
